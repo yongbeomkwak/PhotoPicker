b44cdf05ec6a38d950e65688631d37a27c1d89fe
yongbeomkwak <yongbeomkwak@gmail.com>
2024-01-27 21:05:08 +0900

    ✅ :: PhotoPickerModel tap_MoveToEdit

--- PhotoPickerTests/PhotoPickerViewModelTests.swift
+++ PhotoPickerTests/PhotoPickerViewModelTests.swift
@@ -131,47 +131,6 @@ final class PhotoPickerViewModelTests: XCTestCase {
         .store(in: &subscription)
     }
     
-    
-    func test_tapItem에_이미_선택된_index가_들어오면_isSelected값은_False() {
-        
-        let input = PhotoPickerViewModel.Input()
-        
-        let output = viewModel.transform(input: input)
-        
-        input.fetchData.send([Data(),Data(),Data(),Data(),Data()])
-        
-        // 6개 데이터 소스(0,1,2,3,4,5)
-        // 0은 기본 데이터 + 5개는 추가 데이터
-        
-        input.tapItem.send(1)
-        input.tapItem.send(5)
-        input.tapItem.send(2)
-        
-        //인덱스 1,5,2 데이터 선택
-        //1,5,2의 isSelected = true
-        
-        input.tapItem.send(1)
-        input.tapItem.send(2)
-        
-        //인덱스 1,2 데이터를 재 선택
-        //5의 isSelected = true
-        //나머지는 false
-        
-        output.dataSource.sink { data in
-            for d in data {
-                if  d.index == 5  {
-                    // 1,5,2는 선택된 것이므로 isSelected는 true여야함
-                    XCTAssertTrue(d.isSelected)
-                } else {
-                    DEBUG_LOG("\(d.index) \(d.isSelected)")
-                    XCTAssertFalse(d.isSelected)
-                }
-            }
-            
-        }
-        .store(in: &subscription)
-    }
-    
     func test_tapItem에_이미_있는_데이터가들어오면_selectedItems의_count는_1() {
         
         let input = PhotoPickerViewModel.Input()
@@ -201,6 +160,39 @@ final class PhotoPickerViewModelTests: XCTestCase {
             XCTAssertEqual(data.count, 1)
         }
         .store(in: &subscription)
+    }
+    
+    func test_tapMoveToEdit에_들어오면_final_items의_count는_2() {
+        
+        let input = PhotoPickerViewModel.Input()
+        
+        let output = viewModel.transform(input: input)
+        
+        input.fetchData.send([Data(),Data(),Data(),Data(),Data()])
+        
+        // 6개 데이터 소스(0,1,2,3,4,5)
+        // 0은 기본 데이터 + 5개는 추가 데이터
+        
+        input.tapItem.send(1)
+        input.tapItem.send(5)
+        input.tapItem.send(2)
+        
+        //인덱스 1,5,2 데이터 선택
+        //1,5,2의 isSelected = true
+        
+        input.tapItem.send(1)
+        
+        //인덱스 1데이터를 재 선택
+        // selectedItems = 3-1 = 2
+        
+        input.tapMoveToEdit.send(())
+        
+        output.finalItems.sink { data in
+            
+            XCTAssertEqual(data.count, 2)
+        }
+        .store(in: &subscription)
+        
     }
                
 
